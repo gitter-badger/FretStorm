473f2f02913fdd093aff542765971f20b2947b2a
Félix Piédallu <salamandar@excalibur.home>
2015-12-30 21:03:32 +0100

    manage command line args

--- src/fretstorm.cpp
+++ src/fretstorm.cpp
@@ -15,8 +15,20 @@ using namespace std;
 //#define MIDIFILE "/home/salamandar/FoF/songs/Odino Hero/Tier 8/08.3 Seventh Wonder - Alley Cat/notes.mid"
 //#define MIDIFILE "MusicExamples/Kamelot - Nights of Arabia/notes_orig.mid"
 #define MIDIFILE "/home/salamandar/FoF/NewSongs/Kamelot - Center of the universe/notes.mid"
+string filename;
 
 int main(int argc, char const *argv[]) {
+    if (argc != 2) {
+        cout << "Usage: " << argv[0] << " [-|midifile]" << endl;
+        exit(0);
+    } else {
+        char arg = *argv[1];
+        if (arg == '-')
+            filename = MIDIFILE;
+        else
+            filename = argv[1];
+    }
+
     node root_node(PATH);
     directory root_dir(root_node);
     //root_dir.list_dir();
@@ -27,8 +39,8 @@ int main(int argc, char const *argv[]) {
         << root_dir.count_dir_musics(true)
         << endl;
 
-    MidiFile midifile(MIDIFILE);
-    MidiSong midiSong(MIDIFILE);
+    MidiFile midifile(filename);
+    MidiSong midiSong(filename);
 //    printMidiFile(midifile);
 
     midiSong.print();
