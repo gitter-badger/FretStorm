1ff28333ec623f1f9459d070bf11eeb20dc8dbde
Félix Piédallu <salamandar@excalibur.home>
2015-12-30 01:50:53 +0100

    finished the event parsing, just need to add some small things

--- src/midi/event.h
+++ src/midi/event.h
@@ -9,24 +9,78 @@
 
 using namespace std;
 
+enum e_event_type {
+
+    NOTE_ON  =  90,
+    NOTE_OFF =  80,
+
+    META                = 200,
+    META_TEXT           = 201,
+    META_TRACKNAME      = 203,
+    META_TEMPO_SET      = 281,
+    META_TIME_SIG       = 288,
+    META_END_OF_TRACK   = 247
+};
+
+enum e_notes {
+    Easy_A  = 60,
+    Easy_B  = 61,
+    Easy_C  = 62,
+    Easy_D  = 63,
+    Easy_E  = 64,
+
+    Medium_A = 72,
+    Medium_B = 73,
+    Medium_C = 74,
+    Medium_D = 75,
+    Medium_E = 76,
+
+    Hard_A = 84,
+    Hard_B = 85,
+    Hard_C = 86,
+    Hard_D = 87,
+    Hard_E = 88,
+
+    Expert_A = 96,
+    Expert_B = 97,
+    Expert_C = 98,
+    Expert_D = 99,
+    Expert_E = 100
+};
+
+
+
+
 class FretMidiEvent {
 public:
     FretMidiEvent(MidiEvent& event);
   // ~FretMidiEvent();
+
 
     void print();
 
 protected:
     MidiEvent& event;
 
-    int commandbyte;
+    e_event_type eventType;
 
-    int     note;
+    // Here, only one is not NULL
+    e_notes note;
+    string  text;
+    int     tempo;
+    int     timeSig;
+
 
     int     tick;
     int     track;
     double  seconds;
 
+    void setMetaFromEvent();
+    void setTextFromEvent();
+    void setTempoFromEvent();
+    void setTimeSigFromEvent();
+
+    char* printNote(int note);
 };
 
 #endif
